84f451629b0be9e92d4a9e83b080bdba0a5256a9
philastrio <han_gang@naer.com>
2019-03-06 20:59:57 +0900

    error

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# awesomeStyle
-Styled Components Study
--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+
+createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    }
+`;
 
 class App extends Component {
   render() {
-    return () {
-      <div className="App" />;
-    }
+    return (
+      <Container>
+        <Button>Hello</Button>
+        <Button danger> Hi </Button>
+      </Container>
+    );
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? '#e74c3c' : '#2ecc71')};
+`;
 
 export default App;
